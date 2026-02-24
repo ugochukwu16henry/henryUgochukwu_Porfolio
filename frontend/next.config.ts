69b9e6c0997113325456212a0f3098d5b5f9b1e2
Henry Ugochukwu <ugochukwuhenry16@gmail.com>
2026-02-24 14:09:56 +0100

    update

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,10 +2,15 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
+    domains: ['images.unsplash.com', 'henryugochukwuporfolio-production.up.railway.app'],
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**'
+        hostname: 'images.unsplash.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'henryugochukwuporfolio-production.up.railway.app'
       }
     ]
   }
